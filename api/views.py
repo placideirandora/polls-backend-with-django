73ddc40f81ba46872ee2ad205074449d45f55e1a
Placide IRANDORA <placideirandora@gmail.com>
2020-07-18 14:17:44 +0200

    Refactor to user Generic Views for handling serveral HTTP Methods in a Single View Class

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from rest_framework import generics
 
 from .models import (Poll)
 from .serializers import (PollSerializer)
@@ -13,17 +13,11 @@ class Index(APIView):
         return Response({'message': 'Welcome to the Polls REST API'})
 
 
-class PollList(APIView):
-    def get(self, request):
-        polls = Poll.objects.all()[:20]
-        data = PollSerializer(polls, many=True).data
-
-        return Response(data)
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
 
 
-class PollDetail(APIView):
-    def get(self, request, pk):
-        poll = get_object_or_404(Poll, pk=pk)
-        data = PollSerializer(poll).data
-
-        return Response(data)
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
