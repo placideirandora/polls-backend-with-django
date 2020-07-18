08184f931a617ae75146dd1679ef6ec35602f508
Placide IRANDORA <placideirandora@gmail.com>
2020-07-18 13:15:40 +0200

    Build GetPoll and GetPolls API Endpoints with Pure Django

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api/v1/polls/', views.polls_list, name="polls_list"),
+    path('api/v1/polls/<int:pk>/', views.polls_detail, name="polls_detail")
 ]
