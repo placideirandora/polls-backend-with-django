bc38ec1a73e0abe2677d9122881bf382f8d51562
Placide IRANDORA <placideirandora@gmail.com>
2020-07-18 20:13:45 +0200

    Unit-Test the Polls Endpoint with Authentication for the Test Case to Pass

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 from rest_framework.test import APIRequestFactory
 
@@ -10,9 +12,25 @@ class TestPollEndpoint(APITestCase):
         self.factory = APIRequestFactory()
         self.view = PollViewSet.as_view({'get': 'list'})
         self.uri = 'api/v1/polls/'
+        self.user = self.setup_user()
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+
+        return User.objects.create_user(
+            'test',
+            email='testuser@test.com',
+            password='test'
+        )
 
     def test_list(self):
-        request = self.factory.get(self.uri)
+        request = self.factory.get(self.uri,
+                                   HTTP_AUTHORIZATION='Token {}'
+                                   .format(self.token.key))
+        request.user = self.user
         response = self.view(request)
 
         self.assertEqual(response.status_code, 200,
