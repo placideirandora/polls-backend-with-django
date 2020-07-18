9f74005b6b0bb8619c9d769bf98e83762e7bf420
Placide IRANDORA <placideirandora@gmail.com>
2020-07-18 16:18:51 +0200

    Improve the Endpoints

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,10 @@ from .views import (Index, PollList, PollDetail, ChoiceList, CreateVote)
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
-    path('api/v1/polls/', PollList.as_view(), name="polls_list"),
-    path('api/v1/polls/<int:pk>/', PollDetail.as_view(), name="polls_detail"),
-    path('api/v1/choices/', ChoiceList.as_view(), name="choice_list"),
-    path('api/v1/vote/', CreateVote.as_view(), name="create_vote"),
+    path('api/v1/polls/', PollList.as_view(), name='polls_list'),
+    path('api/v1/polls/<int:pk>/', PollDetail.as_view(), name='polls_detail'),
+    path('api/v1/polls/<int:pk>/choices/',
+         ChoiceList.as_view(), name='choice_list'),
+    path('api/v1/polls/<int:poll_pk>/choices/<int:choice_pk>/vote/',
+         CreateVote.as_view(), name="create_vote"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.status import (HTTP_201_CREATED, HTTP_400_BAD_REQUEST)
 
 from .models import (Poll, Choice)
 from .serializers import (PollSerializer, ChoiceSerializer, VoteSerializer)
@@ -24,9 +25,26 @@ class PollDetail(generics.RetrieveDestroyAPIView):
 
 
 class ChoiceList(generics.ListCreateAPIView):
-    queryset = Choice.objects.all()
     serializer_class = ChoiceSerializer
+
+    def get_queryset(self):
+        queryset = Choice.objects.filter(poll_id=self.kwargs['pk'])
+
+        return queryset
 
 
 class CreateVote(generics.CreateAPIView):
     serializer_class = VoteSerializer
+
+    def post(self, request, poll_pk, choice_pk):
+        voted_by = request.data.get('voted_by')
+        data = {'choice': choice_pk, 'poll': poll_pk, 'voted_by': voted_by}
+        serializer = VoteSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=HTTP_201_CREATED)
+
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
