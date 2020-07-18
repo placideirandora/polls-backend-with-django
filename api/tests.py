c7aec93fb248a841b482618e43f6bfa96a9ae0fe
Placide IRANDORA <placideirandora@gmail.com>
2020-07-18 20:02:17 +0200

    Unit-Test the List Case of the Polls Endpoint

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
 
+from .views import (PollViewSet)
 # Create your tests here.
+
+
+class TestPollEndpoint(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = PollViewSet.as_view({'get': 'list'})
+        self.uri = 'api/v1/polls/'
+
+    def test_list(self):
+        request = self.factory.get(self.uri)
+        response = self.view(request)
+
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
