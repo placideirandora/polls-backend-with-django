8d86436e49a0cdb4fbc4befd3e547b9611d54163
Placide IRANDORA <placideirandora@gmail.com>
2020-07-26 21:41:41 +0200

    Comment the Tests and Fixing the failing one

--- api/tests.py
+++ api/tests.py
@@ -4,12 +4,17 @@ from rest_framework.test import APITestCase
 from rest_framework.test import APIRequestFactory
 from rest_framework.test import APIClient
 
-from .views import (PollViewSet)
+from .views.poll.views import (PollViewSet)
 
 # Create your tests here.
 
 
 class TestPollEndpoint(APITestCase):
+    """
+    Ensure we can setup everything required in advance
+    to running the test cases
+    """
+
     def setUp(self):
         self.client = APIClient()
         self.factory = APIRequestFactory()
@@ -21,15 +26,23 @@ class TestPollEndpoint(APITestCase):
 
     @staticmethod
     def setup_user():
+        """
+        Ensure we can setup a user to be used in the api calls.
+        """
+
         User = get_user_model()
 
         return User.objects.create_user(
-            'test',
+            username='test',
             email='testuser@test.com',
             password='test'
         )
 
     def test_list_with_factory(self):
+        """
+        Ensure we can list poll objects with factory.
+        """
+
         request = self.factory.get(self.uri,
                                    HTTP_AUTHORIZATION='Token {}'
                                    .format(self.token.key))
@@ -41,6 +54,10 @@ class TestPollEndpoint(APITestCase):
                          .format(response.status_code))
 
     def test_list_with_api_client(self):
+        """
+        Ensure we can list poll objects with api client.
+        """
+
         self.client.login(username='test', password='test')
         response = self.client.get(self.uri)
 
@@ -49,12 +66,16 @@ class TestPollEndpoint(APITestCase):
                          .format(response.status_code))
 
     def test_create(self):
+        """
+        Ensure we can create a new poll object.
+        """
+
         self.client.login(username='test', password='test')
-        params = {
+        data = {
             'question': 'What is best car in the world?',
             'created_by': 1
         }
-        response = self.client.post(self.uri, params)
+        response = self.client.post(self.uri, data, format='json')
 
         self.assertEqual(response.status_code, 201,
                          'Expected Response Code 201, received {0} instead.'
